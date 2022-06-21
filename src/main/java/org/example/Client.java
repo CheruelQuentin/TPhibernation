37300b04348fb27d3c31725dbb4d6e57fce7694c
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2022-06-21 11:20:24 +0200

    add Client table

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,73 @@
+package org.example;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "CLIENT",
+        uniqueConstraints = { @UniqueConstraint(columnNames = { "CLI_ID" }) })
+
+public class Client {
+    private Integer id;
+    private String nom;
+    private String prenom;
+    private String telephone;
+
+    public Client(){}
+
+    public Client(Integer id,String nom, String prenom, String telephone){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+    }
+
+
+    @Id
+    @Column(name="CLI_ID")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Id
+    @Column(name="CLI_NOM")
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Id
+    @Column(name="CLI_PRENOM")
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+
+    @Id
+    @Column(name="CLI_TELEPHONE")
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
